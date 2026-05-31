d7cedf0925e694732ee7a44adc4ba31db94d9df0
Rohit Bangal <72252030+rohit-bangal@users.noreply.github.com>
2026-05-31 21:51:24 +0000

    Updated the copyright information in the footer of the blog-single.html file to reflect the correct author and removed references to the original template's design credits.

--- js/modal.js
+++ js/modal.js
@@ -9,14 +9,6 @@ modalBtns.forEach(function(btn){
   }
 });
 
-var modalBtns1 = [...document.querySelectorAll(".btn")];
-modalBtns1.forEach(function(btn){
-  btn.onclick = function() {
-    var modal = btn.getAttribute('data-modal');
-    document.getElementById(modal).style.display = "block";
-  }
-});
-
 var closeBtns = [...document.querySelectorAll(".close")];
 closeBtns.forEach(function(btn){
   btn.onclick = function() {
